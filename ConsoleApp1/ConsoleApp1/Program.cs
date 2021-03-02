89a2fca4710291671f89bc3ed529c6062f76cf53
Nataliia Legka <Nataliia.Legka@teaminternational.com>
2021-03-02 11:08:27 +0200

    + circle

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,14 +13,14 @@ namespace ConsoleApp1
             bool square1Calculated = false;
             for (int i = 0; i < 3; i++)
             {
-                Console.Write("Введите длину стороны квадрата -> ");
+                Console.Write("\nВведите длину стороны квадрата -> ");
                 string sidetext = Console.ReadLine();
                 double squareside = Convert.ToDouble(sidetext);
 
                 if (squareside > 0)
                 {
                     double square1 = squareside * squareside;
-                    Console.WriteLine("Площадь квадрата равна " + Math.Round(square1, 2));
+                    Console.WriteLine("\nПлощадь квадрата равна " + Math.Round(square1, 2));
                     square1Calculated = true;
                     break;
                 }
@@ -32,24 +32,46 @@ namespace ConsoleApp1
             }
             if (!square1Calculated) 
             {
-                Console.WriteLine("Вы исчерпали количество попыток");
+                Console.WriteLine("\nВы исчерпали количество попыток");
                 Random rnd = new Random();
                 double randomSide = rnd.NextDouble()*4.5+0.5;
                 
                 Console.WriteLine("Поэтому будет посчитан случайный квадрат со стороной " + randomSide);
                 double square1 = randomSide * randomSide;
-                Console.WriteLine("Площадь случайного квадрата равна " + Math.Round(square1, 2));
+                Console.WriteLine("\nПлощадь случайного квадрата равна " + Math.Round(square1, 2));
             }
-            
-                
 
 
+            bool square2Calculated = false;
+            for (int i = 0; i < 3; i++)
+            {
 
-            Console.Write("Введите длину радиуса круга -> ");
-            string radtext = Console.ReadLine();
-            double radcircle = Convert.ToDouble(radtext);
-            double square2 = Math.PI * radcircle * radcircle;
-            Console.WriteLine("Площадь круга равна " + Math.Round(square2, 2));
+                Console.Write("\nВведите длину радиуса круга -> ");
+                string radtext = Console.ReadLine();
+                double radcircle = Convert.ToDouble(radtext);
+
+                if (radcircle > 0)
+                {
+                    double square2 = Math.PI * radcircle * radcircle;
+                    Console.WriteLine("\nПлощадь круга равна " + Math.Round(square2, 2));
+                    square2Calculated = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Число должно быть больше нуля");
+                }
+            }
+            if (!square1Calculated)
+            {
+                Console.WriteLine("\nВы исчерпали количество попыток");
+                Random rnd = new Random();
+                double randomRad = rnd.NextDouble() * 4.5 + 0.5;
+
+                Console.WriteLine("Поэтому будет посчитан случайный круг с радиусом " + randomRad);
+                double square1 = Math.PI * randomRad * randomRad;
+                Console.WriteLine("\nПлощадь случайного круга равна " + Math.Round(square1, 2));
+            }
 
 
             Console.ReadKey();
